2ebbf3616e005266ef1ee8737e072cefefcc787f
HugoTodeschini <todeschinihugo@gmail.com>
2019-11-21 22:58:55 +0100

    Ajout d'un deuxieme fichier python

---
+++ PythonProject.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+
+print('Hello World')
